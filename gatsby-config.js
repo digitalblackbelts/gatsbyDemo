945d673dcca63e19274c4e91527fd2dbb7e5f8ed
Thomas K <keremtal@gmail.com>
2023-12-05 12:52:51 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Gdasby-Try-1`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
